0903efb1a7718f4af92844befc4784de211e2351
panawara <pa.nawara@gmail.com>
2017-07-23 13:53:59 +0200

    first exercsie function, printInfo

---
+++ script.js
@@ -0,0 +1,14 @@
+function Phone(brand, price, color) {
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
+}
+Phone.prototype.printInfo = function() {
+	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+var iPhone6S = new Phone("Apple", 2250, "silver");
+iPhone6S.printInfo();
+var GalaxyS6 = new Phone("Samsung", 3500, "grey");
+GalaxyS6.printInfo();
+var One = new Phone("OnePlus", 2000, "black");
+One.printInfo();
