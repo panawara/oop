12c26ad4f35fe0cd5d8225a79cae953030da10d1
panawara <pa.nawara@gmail.com>
2017-07-24 20:53:30 +0200

    warranty cost added

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ function Phone(brand, price, color) {
 	this.brand = brand;
 	this.price = price;
 	this.color = color;
-}
+};
+
 Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+	console.log('The phone brand is: ', this.brand, ', color is: ', this.color, ', price: ', this.price, '$'
+	, ', Extend warranty: ', this.getWarrantyCost(),'$');
+};
+
+Phone.prototype.getWarrantyCost = function() {
+	return this.price * 0.1;
 }
+
 var iPhone6S = new Phone("Apple", 2250, "silver");
 iPhone6S.printInfo();
 var GalaxyS6 = new Phone("Samsung", 3500, "grey");
